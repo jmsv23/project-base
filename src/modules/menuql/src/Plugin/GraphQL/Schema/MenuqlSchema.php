6054faf68a2054fa99d114ec13e412b0cf8e73af
jmsv23 <jmsv23@gmail.com>
2020-07-25 14:56:17 -0500

    graphql init

--- src/modules/menuql/src/Plugin/GraphQL/Schema/MenuqlSchema.php
+++ src/modules/menuql/src/Plugin/GraphQL/Schema/MenuqlSchema.php
@@ -5,7 +5,8 @@ namespace Drupal\menuql\Plugin\GraphQL\Schema;
 use Drupal\graphql\GraphQL\ResolverBuilder;
 use Drupal\graphql\GraphQL\ResolverRegistry;
 use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
-use Drupal\menuql\Wrappers\QueryConnection;
+use Drupal\node\NodeInterface;
+// use Drupal\Core\Plugin\Contex\ContextDefinition;
 
 /**
  * @Schema(
@@ -22,51 +23,34 @@ class MenuqlSchema extends SdlSchemaPluginBase {
     $builder = new ResolverBuilder();
     $registry = new ResolverRegistry();
 
-    $this->addQueryFields($registry, $builder);
-    $this->addMenuFields($registry, $builder);
+    // Tell GraphQL how to resolve types of a common interface.
+    $registry->addTypeResolver('NodeInterface', function ($value) {
+      if ($value instanceof NodeInterface) {
+        switch($value->bundle()) {
+          case 'menu': return 'Menu';
+        }
+      }
+      throw new Error('Could not resolve content type.');
+    });
 
-    return $registry;
-  }
-
-  /**
-   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
-   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
-   */
-  protected function addMenuFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver('Menu', 'id',
-      $builder->produce('entity_id')
-        ->map('entity', $builder->fromParent())
-    );
-
-    $registry->addFieldResolver('Menu', 'name',
-      $builder->compose(
-        $builder->produce('entity_label')
-          ->map('entity', $builder->fromParent()),
-        $builder->produce('uppercase')
-          ->map('string', $builder->fromParent())
-      )
-    );
-
-    $registry->addFieldResolver('Menu', 'description',
-      $builder->compose(
-        $builder->produce('entity_owner')
-          ->map('entity', $builder->fromParent()),
-        $builder->produce('entity_label')
-          ->map('entity', $builder->fromParent())
-      )
-    );
-  }
-
-  /**
-   * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
-   * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
-   */
-  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder) {
     $registry->addFieldResolver('Query', 'menu',
       $builder->produce('entity_load')
         ->map('type', $builder->fromValue('node'))
         ->map('bundles', $builder->fromValue(['menu']))
         ->map('id', $builder->fromArgument('id'))
     );
+
+    $registry->addFieldResolver('Menu', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Menu', 'title',
+      $builder->produce('entity_label')
+        ->map('entity', $builder->fromParent())
+    );
+
+    return $registry;
   }
+
 }
