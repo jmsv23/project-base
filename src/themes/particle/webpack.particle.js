4867b65a7898e7dcc591d84746f1b7bb274b4eb7
jmsv23 <jmsv23@gmail.com>
2020-07-29 09:49:15 -0500

    add sass tokens

--- src/themes/particle/webpack.particle.js
+++ src/themes/particle/webpack.particle.js
@@ -69,6 +69,9 @@ module.exports = {
           },
           {
             loader: 'sass-loader',
+            options: {
+              additionalData: '@import "~tokens/sass/tokens";',
+            }
           },
         ],
       },
